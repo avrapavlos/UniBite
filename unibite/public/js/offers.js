3a58f37b91beb46df47ac45f4de08c8d4a18e7f5
RubenKingCP <redrubenking@gmail.com>
2026-07-16 22:37:52 +0300

    Work on browse offers page

---
+++ unibite/public/js/offers.js
@@ -0,0 +1,47 @@
+let offers = [];
+
+
+fetch("../../data/offers.json")
+.then(response => response.json())
+.then(data => {
+
+    offers = data;
+
+    displayOffers(offers);
+});
+
+function displayOffers(data){
+
+    const container = document.querySelector(".offer-list");
+
+    container.innerHTML = "";
+
+
+    data.forEach(offer => {
+
+
+        const card = document.createElement("article");
+
+        card.classList.add("offer-card");
+
+
+        // αν δεν υπάρχουν μερίδες
+        if(offer.portions === 0){
+            card.classList.add("empty");
+        }
+
+
+        card.innerHTML = `
+            <h2>${offer.title}</h2>
+            <p>${offer.description}</p>
+            <p>
+                Μερίδες: ${offer.portions}
+            </p>
+        `;
+
+
+        container.appendChild(card);
+
+    });
+
+}
