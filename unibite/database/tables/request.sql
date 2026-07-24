1b494f82c16eeb6600c8419a2ca7b31e0fda23f5
avrapavlos <167622014+avrapavlos@users.noreply.github.com>
2026-07-24 15:11:04 +0300

    Last table changes for now

--- unibite/database/tables/request.sql
+++
@@ -1,8 +0,0 @@
-use unibite;
-
-DROP TABLE IF EXISTS requests
-
-CREATE TABLE requests(
-    ad_id : INT,
-    con_id : INT
-);
--- unibite/database/tables/requestTable.sql
+++ unibite/database/tables/requestTable.sql
@@ -3,6 +3,10 @@ use unibite;
 DROP TABLE IF EXISTS requests
 
 CREATE TABLE requests(
-    ad_id : INT,
-    con_id : INT
+    ADD CONSTRAINT fk_advertisment,
+    FOREIGN KEY(ad_id),
+    REFERENCES advertisments(ad_id),
+    ADD CONSTRAINT fk_consumer,
+    FOREIGN KEY(con_id),
+    REFERENCES users(user_id)
 );
