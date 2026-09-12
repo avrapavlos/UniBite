ed11ea19644116e8abaf8b4c3734984e878a6358
Kostas Laios <redrubenking@gmail.com>
2026-09-12 19:50:08 +0300

    Fixed small point bug

--- unibite/server/controllers/authController.js
+++ unibite/server/controllers/authController.js
@@ -104,7 +104,7 @@ export async function register(req, res) {
                 username: name,
                 name,
                 email,
-                points: 50,
+                points: 5,
                 portions_given: 0,
                 portions_received: 0,
                 profile_pic: null,
